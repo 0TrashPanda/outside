05c76fc0f92e8da7db63f37522b4b38aab1ea0ec
@0TrashPanda <jonah.jvdb@gmail.com>
2023-12-03 22:49:36 +0100

    rocky atributes

--- src/main/java/outside/Outside.java
+++ src/main/java/outside/Outside.java
@@ -1,12 +1,30 @@
 package outside;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import outside.common.item.ItemRock;
 import outside.common.registries.OutsideItems;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 public class Outside implements ModInitializer {
+
+    private static final ItemGroup OUTSIDE_GROUP = FabricItemGroup.builder()
+    .icon(() -> new ItemStack(OutsideItems.ROCK))
+    .displayName(Text.translatable("itemGroup.outside"))
+        .entries((context, entries) -> {
+        entries.add(OutsideItems.ROCK);
+    })
+    .build();
 
     @Override
     public void onInitialize() {
         new OutsideItems();
+        Registry.register(Registries.ITEM_GROUP, new Identifier( "outside"), OUTSIDE_GROUP);
     }
 }
--- src/main/java/outside/common/item/ItemRock.java
+++ src/main/java/outside/common/item/ItemRock.java
@@ -1,7 +1,14 @@
 package outside.common.item;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,8 +23,19 @@ public class ItemRock extends Item {
 
     Random rand =  new Random();
 
+    public static final float ATTACK_DAMAGE = 1.0f;
+    public static final float ATTACK_SPEED = -2.5f;
+    public static final int DURABILITY = 32;
+
+
+    private Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+
     public ItemRock(Settings settings) {
-        super(settings.maxDamage(32));
+        super(settings.maxDamage(DURABILITY));
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Tool modifier", ATTACK_DAMAGE, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Tool modifier", ATTACK_SPEED, EntityAttributeModifier.Operation.ADDITION));
+        this.attributeModifiers = builder.build();
     }
 
     @Override
@@ -43,5 +61,13 @@ public class ItemRock extends Item {
         }
         int result = sum + rand.nextInt(damage == 0 ? 1 : damage);
         return result;
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        if (slot == EquipmentSlot.MAINHAND) {
+            return this.attributeModifiers;
+        }
+        return super.getAttributeModifiers(slot);
     }
 }
