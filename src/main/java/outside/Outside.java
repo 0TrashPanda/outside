32dda7b8d022aacdc4bc31f9b0da29786b6436c3
@0TrashPanda <jonah.jvdb@gmail.com>
2023-12-04 02:23:25 +0100

    lang++

--- src/main/java/outside/Outside.java
+++ src/main/java/outside/Outside.java
@@ -2,12 +2,10 @@ package outside;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import outside.common.item.ItemRock;
 import outside.common.registries.OutsideItems;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
